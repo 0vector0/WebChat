9657ea0363f2f5d339aec19f56c5cdf22569e8d7
0vector0 <0vector0@gmail.com>
2016-11-10 20:52:08 +0200

    use app.properties in DataConfig.java

--- src/main/java/com/github/mykhalechko/webchat/util/DataConfig.java
+++ src/main/java/com/github/mykhalechko/webchat/util/DataConfig.java
@@ -25,14 +25,23 @@ import java.util.Properties;
 @EnableJpaRepositories("com.github.mykhalechko.webchat.repository")
 public class DataConfig {
 
+    //    DB properties:
     private static final String PROP_DATABASE_DRIVER = "db.driver";
-    private static final String PROP_DATABASE_PASSWORD = "db.password";
     private static final String PROP_DATABASE_URL = "db.url";
     private static final String PROP_DATABASE_USERNAME = "db.username";
+    private static final String PROP_DATABASE_PASSWORD = "db.password";
+
+    //    Hibernate Configuration:
     private static final String PROP_HIBERNATE_DIALECT = "db.hibernate.dialect";
     private static final String PROP_HIBERNATE_SHOW_SQL = "db.hibernate.show_sql";
-    private static final String PROP_ENTITYMANAGER_PACKAGES_TO_SCAN = "db.entitymanager.packages.to.scan";
     private static final String PROP_HIBERNATE_HBM2DDL_AUTO = "db.hibernate.hbm2ddl.auto";
+    private static final String PROP_HIBERNATE_CONNECTION_CHARSET = "db.hibernate.connection.CharSet";
+    private static final String PROP_HIBERNATE_CONNECTION_CHARACTERENCODING =
+            "db.hibernate.connection.characterEncoding";
+    private static final String PROP_HIBERNATE_CONNECTION_USEUNICODE = "db.hibernate.connection.useUnicode";
+
+    //    EntityManager Configuration:
+    private static final String PROP_ENTITYMANAGER_PACKAGES_TO_SCAN = "db.entitymanager.packages.to.scan";
 
     @Resource
     private Environment env;
@@ -40,7 +49,6 @@ public class DataConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
         dataSource.setDriverClassName(env.getRequiredProperty(PROP_DATABASE_DRIVER));
         dataSource.setUrl(env.getRequiredProperty(PROP_DATABASE_URL));
         dataSource.setUsername(env.getRequiredProperty(PROP_DATABASE_USERNAME));
@@ -48,14 +56,14 @@ public class DataConfig {
 
         return dataSource;
     }
+
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        //entityManagerFactoryBean.setPersistenceUnitName("WebChat");
         entityManagerFactoryBean.setDataSource(dataSource());
         entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
         entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(PROP_ENTITYMANAGER_PACKAGES_TO_SCAN));
-
         entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
 
         return entityManagerFactoryBean;
@@ -71,16 +79,28 @@ public class DataConfig {
 
     private Properties getHibernateProperties() {
         Properties properties = new Properties();
-        properties.put("hibernate.connection.driver_class", "org.postgresql.Driver");
-        properties.put("hibernate.connection.username", "postgres");
-        properties.put("hibernate.connection.password", "root");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL95Dialect");
-        properties.put("hibernate.show_sql", "true");
-        properties.put("hibernate.hbm2ddl.auto", "update");
-        properties.put("hibernate.connection.url", "jdbc:postgresql://localhost/webchat");
-        properties.put("hibernate.connection.CharSet", "utf8");
-        properties.put("hibernate.connection.characterEncoding", "utf8");
-        properties.put("hibernate.connection.useUnicode", "true");
+
+//        properties.put("hibernate.connection.driver_class", "db.driver");
+//        properties.put("hibernate.connection.username", "postgres");
+//        properties.put("hibernate.connection.password", "root");
+//        properties.put("hibernate.connection.url", "jdbc:postgresql://localhost/webchat");
+
+//        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL95Dialect");
+//        properties.put("hibernate.show_sql", "true");
+//        properties.put("hibernate.hbm2ddl.auto", "update");
+//        properties.put("hibernate.connection.CharSet", "utf8");
+//        properties.put("hibernate.connection.characterEncoding", "utf8");
+//        properties.put("hibernate.connection.useUnicode", "true");
+
+        properties.put("hibernate.dialect", env.getRequiredProperty(PROP_HIBERNATE_DIALECT));
+        properties.put("hibernate.show_sql", env.getRequiredProperty(PROP_HIBERNATE_SHOW_SQL));
+        properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty(PROP_HIBERNATE_HBM2DDL_AUTO));
+        properties.put("hibernate.connection.CharSet", env.getRequiredProperty(PROP_HIBERNATE_CONNECTION_CHARSET));
+        properties.put("hibernate.connection.characterEncoding",
+                env.getRequiredProperty(PROP_HIBERNATE_CONNECTION_CHARACTERENCODING));
+        properties.put("hibernate.connection.useUnicode",
+                env.getRequiredProperty(PROP_HIBERNATE_CONNECTION_USEUNICODE));
+
         return properties;
     }
 

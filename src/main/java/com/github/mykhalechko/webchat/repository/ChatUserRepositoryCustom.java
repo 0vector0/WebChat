bf2ad57375bd75bf21aff0dfb93b0ac3db2ba055
0vector0 <0vector0@gmail.com>
2016-11-12 18:21:07 +0200

    chat user repository

---
+++ src/main/java/com/github/mykhalechko/webchat/repository/ChatUserRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.github.mykhalechko.webchat.repository;
+
+
+import com.github.mykhalechko.webchat.dto.ChatUserDto;
+
+public interface ChatUserRepositoryCustom  {
+
+    public boolean isUserExist(ChatUserDto chatUserDto);
+
+}
--- src/main/java/com/github/mykhalechko/webchat/repository/impl/ChatUserRepositoryImpl.java
+++ src/main/java/com/github/mykhalechko/webchat/repository/impl/ChatUserRepositoryImpl.java
@@ -1,16 +1,20 @@
 package com.github.mykhalechko.webchat.repository.impl;
 
 import com.github.mykhalechko.webchat.dto.ChatUserDto;
-import com.github.mykhalechko.webchat.entity.ChatUser;
 import com.github.mykhalechko.webchat.repository.ChatUserRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.github.mykhalechko.webchat.repository.ChatUserRepositoryCustom;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ChatUserRepositoryImpl implements JpaRepository<ChatUser, Long>, ChatUserRepository {
+public class ChatUserRepositoryImpl implements ChatUserRepositoryCustom {
+
+    @Autowired
+    ChatUserRepository chatUserRepository;
 
     @Override
     public boolean isUserExist(ChatUserDto chatUserDto) {
+
         return false;
     }
 }

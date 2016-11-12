c0c7d11eacd35c9c5b18d1a4d3e79c108eb11c27
DethGhost <admin@deth.org.ua>
2016-11-12 18:42:57 +0200

    really magic

--- src/main/java/com/github/mykhalechko/webchat/repository/impl/ChatUserRepositoryImpl.java
+++ src/main/java/com/github/mykhalechko/webchat/repository/impl/ChatUserRepositoryImpl.java
@@ -2,11 +2,22 @@ package com.github.mykhalechko.webchat.repository.impl;
 
 
 import com.github.mykhalechko.webchat.dto.ChatUserDto;
+import com.github.mykhalechko.webchat.entity.ChatUser;
+import com.github.mykhalechko.webchat.repository.ChatUserRepository;
 import com.github.mykhalechko.webchat.repository.ChatUserRepositoryCustom;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ChatUserRepositoryImpl implements ChatUserRepositoryCustom {
+
+    @Autowired
+    private ChatUserRepository chatUserRepository;
+
     @Override
     public boolean isUserExist(ChatUserDto chatUserDto) {
+        ChatUser u = chatUserRepository.getUserByLogin(chatUserDto.getLogin());
+        if (u.getPassword().equals(chatUserDto.getPassword())) {
+            return true;
+        }
         return false;
     }
 }
---
+++ src/main/webapp/WEB-INF/views/chat.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: DethGhost
+  Date: 12.11.2016
+  Time: 18:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Chat</h1>
+</body>
+</html>

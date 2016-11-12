6f5a91ac3e39928010dbb729098ca2d8b10dc0d4
DethGhost <admin@deth.org.ua>
2016-11-12 18:28:54 +0200

    really magic

--- src/main/java/com/github/mykhalechko/webchat/service/LoginServiceImpl.java
+++ src/main/java/com/github/mykhalechko/webchat/service/LoginServiceImpl.java
@@ -2,17 +2,13 @@ package com.github.mykhalechko.webchat.service;
 
 import com.github.mykhalechko.webchat.dto.ChatUserDto;
 import com.github.mykhalechko.webchat.repository.ChatUserRepository;
-import com.github.mykhalechko.webchat.repository.ChatUserRepositoryCustom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginServiceImpl implements LoginService {
     @Autowired
-    private ChatUserRepositoryCustom chatUserRepository;
-
-    @Autowired
-    private ChatUserRepository chatUserRepositoryOrigin;
+    private ChatUserRepository chatUserRepository;
 
     @Override
     public boolean verifyLogin(ChatUserDto chatUserDto) {

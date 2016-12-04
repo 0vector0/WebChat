0bab8f3d59021f1b21f0eabe6774641d4ba43242
0vector0 <0vector0@gmail.com>
2016-12-04 12:27:06 +0200

    "urlpath", "/getLoginLink"

--- src/main/test/com/github/mykhalechko/webchat/controller/LoginControllerTest.java
+++ src/main/test/com/github/mykhalechko/webchat/controller/LoginControllerTest.java
@@ -1,15 +1,12 @@
 package com.github.mykhalechko.webchat.controller;
 
-import com.github.mykhalechko.webchat.service.LoginService;
 import com.github.mykhalechko.webchat.util.ApplicationConfig;
-import com.github.mykhalechko.webchat.util.JSPConfig;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,9 +14,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -43,7 +42,9 @@ public class LoginControllerTest {
     @Test
     public void getLogin() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/login"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(model()
+                        .attribute("urlpath", "/getLoginLink"));
     }
 
 }
